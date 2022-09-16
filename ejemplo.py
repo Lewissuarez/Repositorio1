78c104a261bc20fecea11471e863d92ef812da3c
suarez <suarezlewis97@gmai.com>
2022-09-16 18:19:16 -0500

    actualizado

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,6 @@
 clientes = {}
 opcion = ''
+nuevoPrecio = ''
 while opcion != '6':
     if opcion == '1':
         codigo = input('codigo de la fruta: ')
@@ -25,15 +26,16 @@ while opcion != '6':
     if opcion == '4':
         print("editar precio")
         codigo = input('codigo de la fruta: ')
-        for codigo in range(len(clientes)):
-            if cliente[precio] == codigo:
-                precio[codigo] = int(input("nuevo precio: "))
-        print("precio editado")
+        for codigo in clientes:
+           for key,values in cliente.items():
+                        if(values == precio):
+                            nuevoPrecio=int(input("Ingrese el nuevo precio: "))
+                            cliente['precio'] = nuevoPrecio
 
 
     if opcion == '5':
         print('Lista de frutas')
-        for clave, valor in clientes.items():
-            print(clave, valor ['nombre'])
+        for clave, valor in clientes[codigo].items():
+            print(clave.title() + ':', valor)
     
     opcion = input('Menú de SuperMercado\n1 Comprar frutas\n2 Eliminar frutas\n3 Mostrar frutas\n4 Editar precio\n5 Lista de frutas\n6 Terminar\nElige una opción:')
